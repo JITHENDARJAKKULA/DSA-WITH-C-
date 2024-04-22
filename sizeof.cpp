a135b93dcb53d08794493541d4f7319174c162e3
JITHENDARJAKKULA <148989477+JITHENDARJAKKULA@users.noreply.github.com>
2024-04-22 13:25:37 +0530

    Add files via upload

---
+++ sizeof.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+ int a;
+   a=19;
+   cout<< "size of int:"<< sizeof(a) <<endl;
+ float b;
+   cout<< "size of float:"<< sizeof(b)<<endl ;
+ char c;
+   cout<< "size of char:"<< sizeof(c)<<endl; 
+ double d;
+   cout<< "size of double:"<< sizeof(d)<<endl;
+ bool y;
+   cout<< "size of bool:"<< sizeof(b)<<endl;
+ short int si;
+   cout<< "size of si:"<<sizeof(si)<<endl;
+ long int li;
+   cout<< "size of li: "<<sizeof(si);
+  return 0;
+ 
+}
